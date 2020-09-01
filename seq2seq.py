03241f6e69a3f60ecf2d3a3c2b1ffe64528b7a98
Yuohan Lee <derq3278@gmail.com>
2020-09-01 16:46:46 -0700

    Change variable name and class parameters

--- seq2seq.py
+++ seq2seq.py
@@ -48,7 +48,7 @@ class LuongEncoder(nn.Module):
         self.embedding = nn.Embedding(input_dim, embed_dim)
         self.rnn = nn.LSTM(embed_dim, hidden_dim, num_layers)
 
-    def forward(self, input_tensor):
+    def forward(self, input):
         """
         Parameters
         ----------
@@ -59,41 +59,41 @@ class LuongEncoder(nn.Module):
         output: tensor (input_length, batch_size, hidden_dim)
         hidden: tensor (num_layers, batch_size, hidden_dim)
         """
-        # embedded (input_length, batch_size, embed_dim)
-        embedded = self.embedding(input_tensor)
-        output, hidden = self.rnn(embedded)
+        # embed (input_length, batch_size, embed_dim)
+        embed = self.embedding(src)
+        output, hidden = self.rnn(embed)
 
         return output, hidden
 
 class AttentionLayer(nn.Module):
 
-    def __init__(self, name, hidden_size, attn_size):
+    def __init__(self, name, hidden_dim, attn_dim):
         super(AttentionLayer, self).__init__()
 
-        if name in ['add', 'additive']:
-            self.w1 = nn.Linear(hidden_size, attn_size)
-            self.w2 = nn.Linear(hidden_size, attn_size)
-            self.v = nn.Linear(attn_size, 1)
+        if name == 'add':
+            self.w1 = nn.Linear(hidden_dim, attn_dim)
+            self.w2 = nn.Linear(hidden_dim, attn_dim)
+            self.v = nn.Linear(attn_dim, 1)
             self.score = self.additive_score
 
         elif name == 'dot':
             self.score = self.dot_score
 
-        elif name in ['general', 'mul', 'multiplicative']:
-            self.w = nn.Linear(hidden_size, hidden_size)
+        elif name in ['general', 'gen']:
+            self.w = nn.Linear(hidden_dim, hidden_dim)
             self.score = self.general_score
 
-        elif name == 'concat':
-            self.w = nn.Linear(hidden_size*2, attn_size)
-            self.v = nn.Linear(attn_size, 1)
+        elif name in ['concat', 'cat']:
+            self.w = nn.Linear(hidden_dim*2, attn_dim)
+            self.v = nn.Linear(attn_dim, 1)
             self.score = self.concat_score
 
         else:
             raise NotImplemented()
 
-    # query: tensor (batch_size, hidden_size)
+    # query: tensor (batch_size, hidden_dim)
     # hidden state of decoder (previous or current)
-    # values: tensor (input_length, batch_size, hidden_size)
+    # values: tensor (input_length, batch_size, hidden_dim)
     # hidden state of input sequences from encoder
     def additive_score(self, query, values):
         return self.v(torch.tanh(self.w1(query.unsqueeze(0)) + self.w2(values))).squeeze(2)
@@ -108,31 +108,31 @@ class AttentionLayer(nn.Module):
         query = query.expand(values.size(0), -1, -1)
         return self.v(torch.tanh(self.w(torch.cat((query, values),dim=2)))).squeeze(2)
 
-    def forward(self, decoder_hidden, encoder_outputs):
+    def forward(self, dec_hidden, enc_outputs):
         """
         Parameters
         ----------
-        decoder_hidden: tensor (batch_size, hidden_size)
-        encoder_outputs: tensor (input_length, batch_size, hidden_size)
+        dec_hidden: tensor (batch_size, hidden_dim)
+        enc_outputs: tensor (input_length, batch_size, hidden_dim)
 
         Return
         ------
-        conext: tensor (batch_size, hidden_size)
+        conext: tensor (batch_size, hidden_dim)
         score: tensor (input_length, batch_size)
         """
         # calculate attention score
         # score (input_length, batch_size)
-        score = self.score(decoder_hidden, encoder_outputs)
+        score = self.score(dec_hidden, enc_outputs)
         # align (batch_size, input_length)
         align = F.softmax(score.t(), dim=1)
-        # context vector (batch_size, 1, hidden_size)
-        context = torch.bmm(align.unsqueeze(1), encoder_outputs.transpose(0, 1))
+        # context vector (batch_size, 1, hidden_dim)
+        context = torch.bmm(align.unsqueeze(1), enc_outputs.transpose(0, 1))
         return context, align
 
 class BahdanauDecoder(nn.Module):
     """ Bahdanau-style decoder
     """
-    def __init__(self, attn_name, output_size, embed_size, hidden_size, attn_dim):
+    def __init__(self, attn_name, output_dim, embed_dim, hidden_dim, attn_dim):
         super(BahdanauDecoder, self).__init__()
 
         self.attn_name = attn_name
@@ -146,31 +146,31 @@ class BahdanauDecoder(nn.Module):
         self.rnn = nn.GRU((embed_dim+hidden_dim*2), hidden_dim)
         self.out = nn.Linear((hidden_dim*2+hidden_dim+embed_dim), output_dim)
 
-    def forward(self, input, hidden, encoder_outputs):
+    def forward(self, input, hidden, enc_outputs):
         """
         Parameters
         ----------
         input: tensor (1, batch_size)
-        hidden: tensor (num_layers, batch_size, hidden_size)
-        encoder_outputs: tensor (input_length, batch_size, hidden_size)
+        hidden: tensor (num_layers, batch_size, hidden_dim)
+        enc_outputs: tensor (input_length, batch_size, hidden_dim)
 
         Return
         ------
-        output: tensor (batch_size, hidden_size)
-        hidden: tensor (num_layers, batch_size, hidden_size)
+        output: tensor (batch_size, hidden_dim)
+        hidden: tensor (num_layers, batch_size, hidden_dim)
         score: tensor (input_length, batch_size)
         """
-        context, score = self.attn(hidden[-1], encoder_outputs)
+        context, score = self.attn(hidden[-1], enc_outputs)
 
         # embed (1, batch_size, embed_dim)
         embed = self.embedding(input)
 
         # concat context and embedded
-        # (1, batch_size, hidden_size+embed_size)
+        # (1, batch_size, hidden_dim+embed_dim)
         concat_output = torch.cat((context.transpose(0,1), embed), dim=2)
 
-        # rnn (1, batch_size, hidden_size)
-        # hidden (num_layers*num_directions, batch_size, hidden_size)
+        # rnn (1, batch_size, hidden_dim)
+        # hidden (num_layers*num_directions, batch_size, hidden_dim)
         rnn_output, hidden = self.rnn(concat_output, hidden)
 
         # output (1, batch_size, output_dim)
@@ -195,13 +195,13 @@ class LuongDecoder(nn.Module):
         self.concat = nn.Linear(hidden_dim*2, hidden_dim)
         self.out = nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, input, hidden, encoder_outputs):
+    def forward(self, input, hidden, enc_outputs):
         """
         Parameters
         ----------
         input: tensor (1, batch_size)
         hidden: tensor (num_layers*num_directions, batch_size, hidden_size)
-        encoder_outputs: tensor (input_length, batch_size, hidden_size)
+        enc_outputs: tensor (input_length, batch_size, hidden_size)
 
         Return
         ------
@@ -209,14 +209,14 @@ class LuongDecoder(nn.Module):
         hidden: tensor (num_layers*num_directions, batch_size, hidden_size)
         score: tensor (input_length, batch_size)
         """
-        # embedded (1, batch_size, embed_size)
-        embedded = self.embedding(input)
+        # embed (1, batch_size, embed_size)
+        embed = self.embedding(input)
 
         # rnn (1, batch_size, hidden_size)
-        rnn_output, hidden = self.rnn(embedded, hidden)
+        rnn_output, hidden = self.rnn(embed, hidden)
 
         # get context vector
-        context, score = self.attention(rnn_output[0], encoder_outputs)
+        context, score = self.attention(rnn_output[0], enc_outputs)
 
         # concat (batch_size, hidden_size)
         attn_hidden = torch.tanh(self.concat(torch.cat((rnn_output[0], context.squeeze(1)), 1)))
@@ -228,11 +228,11 @@ class LuongDecoder(nn.Module):
 
 class Seq2seq(nn.Module):
 
-    def __init__(self, style, attn_name, input_dim, output_dim, 
-                embed_dim, hidden_dim, attn_dim, num_layers, sos_token, device):
+    def __init__(self, name, attention, tgt_sos, input_dim, output_dim, 
+                embed_dim, hidden_dim, attn_dim, num_layers)
         super(Seq2seq, self).__init__()
 
-        self.style = style
+        self.name = name
         self.attn_name = attn_name
         self.input_dim = input_dim
         self.output_dim = output_dim
@@ -240,42 +240,40 @@ class Seq2seq(nn.Module):
         self.hidden_dim = hidden_dim
         self.attn_dim = attn_dim
         self.num_layers = num_layers
-        self.sos_token = sos_token
-
-        self.device = device
+        self.tgt_sos = tgt_sos
 
         if style == 'Bahdanau':
             self.encoder = BahdanauEncoder(input_dim, embed_dim, hidden_dim)
-            self.decoder = BahdanauDecoder(attn_name, output_dim, embed_dim, hidden_dim, attn_dim)
+            self.decoder = BahdanauDecoder(attention, output_dim, embed_dim, hidden_dim, attn_dim)
         elif style == 'Luong':
             self.encoder = LuongEncoder(input_dim, embed_dim, hidden_dim, num_layers)
-            self.decoder = LuongDecoder(attn_name, output_dim, embed_dim, hidden_dim, attn_dim, num_layers)
+            self.decoder = LuongDecoder(attention, output_dim, embed_dim, hidden_dim, attn_dim, num_layers)
         else:
             raise ValueError()
 
-    def forward(self, src, trg):
+    def forward(self, src, tgt):
 
-        target_length = trg.size(0)
+        target_length = tgt.size(0)
         batch_size = src.size(1)
 
         # encoder
-        encoder_outputs, encoder_hidden = self.encoder(src)
+        enc_outputs, enc_hidden = self.encoder(src)
 
         # decoder
-        decoder_outputs = []
+        dec_outputs = []
         attentions = []
 
-        decoder_input = torch.tensor(self.sos_token, dtype=torch.long, device=self.device).expand(1, batch_size)
-        decoder_hidden = encoder_hidden
+        dec_input = torch.tensor(self.tgt_sos, dtype=torch.long, device=tgt.device).expand(1, batch_size)
+        dec_hidden = enc_hidden
 
         for i in range(1, target_length):
-            decoder_output, decoder_hidden, attention = \
-                self.decoder(decoder_input, decoder_hidden, encoder_outputs)
+            dec_output, dec_hidden, attention = \
+                self.decoder(dec_input, dec_hidden, enc_outputs)
 
-            topv, topi = decoder_output.topk(1, dim=1)
-            decoder_input = topi.detach().t()
+            topv, topi = dec_output.topk(1, dim=1)
+            dec_input = topi.detach().t()
 
-            decoder_outputs.append(decoder_output)
+            dec_outputs.append(dec_output)
             attentions.append(attention.squeeze())
 
         #(target_length, batch_size, output_size), (target_length, batch_size, input_length)
@@ -285,14 +283,17 @@ class Seq2seq(nn.Module):
 
         state = {
             'state_dict': self.state_dict(),
-            'style': self.style,
-            'attention_name': self.attn_name,
-            'input_dim': self.input_dim,
-            'output_dim': self.output_dim,
-            'embedding_dim': self.embed_dim,
-            'hidden_dim': self.hidden_dim,
-            'attn_dim': self.attn_dim,
-            'num_layers': self.num_layers,
+            'parameter': {
+                'name': self.name,
+                'attn_name': self.attn_name,
+                'tgt_sos': self.tgt_sos,
+                'input_dim': self.input_dim,
+                'output_dim': self.output_dim,
+                'embedding_dim': self.embed_dim,
+                'hidden_dim': self.hidden_dim,
+                'attn_dim': self.attn_dim,
+                'num_layers': self.num_layers,
+            }
             'input_lang': input_lang,
             'target_lang': target_lang
         }
@@ -305,16 +306,8 @@ class Seq2seq(nn.Module):
 
         state = torch.load(state_path, map_location=device)
 
-        dec_name = state['style']
-        attn_name = state['attention_name']
-        attn_size = state['attn_dim']
-        embed_size = state['embedding_dim']
-        hidden_size = state['hidden_dim']
-        input_size = state['input_dim']
-        output_size = state['output_dim']
-        num_layrs = state['num_layers']
-
-        model = cls(dec_name, attn_name, input_size, output_size, embed_size, hidden_size, attn_size, num_layers, device).to(device)
+        params = state['parameter']
+        model = cls(**params).to(device)
         model.load_state_dict(state['state_dict'])
 
         return model, state['input_lang'], state['target_lang']
--- transformer.py
+++ transformer.py
@@ -5,12 +5,12 @@ import torch.nn.functional as F
 
 class PositionalEncoding(nn.Module):
 
-    def __init__(self, d_model, max_len):
+    def __init__(self, model_dim, max_len):
         super(PositionalEncoding, self).__init__()
 
-        pe = torch.zeros(max_len, d_model)
+        pe = torch.zeros(max_len, model_dim)
         pos = torch.arange(0, max_len).unsqueeze(1)
-        term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        term = torch.exp(torch.arange(0, model_dim, 2) * -(math.log(10000.0) / model_dim))
 
         pe[:, 0::2] = torch.sin(pos * term)
         pe[:, 1::2] = torch.cos(pos * term)
@@ -22,42 +22,42 @@ class PositionalEncoding(nn.Module):
 
 class PositionWiseFullyConnectedLayer(nn.Module):
 
-    def __init__(self, d_model, d_ff):
+    def __init__(self, model_dim, ff_dim):
         super(PositionWiseFullyConnectedLayer, self).__init__()
 
-        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc1 = nn.Linear(model_dim, ff_dim)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(d_ff, d_model)
+        self.fc2 = nn.Linear(ff_dim, model_dim)
 
     def forward(self, x):
         return self.fc2(self.relu(self.fc1(x)))
 
 class MultiHeadAttentionLayer(nn.Module):
 
-    def __init__(self, d_model, heads):
+    def __init__(self, model_dim, num_heads):
         super(MultiHeadAttentionLayer, self).__init__()
 
-        assert d_model % heads == 0
-        self.d_model = d_model
-        self.heads = heads
-        self.d_k = d_model // heads
+        assert model_dim % num_heads == 0
+        self.model_dim = model_dim
+        self.num_heads = num_heads
+        self.d_k = model_dim // num_heads
 
-        self.fc_q = nn.Linear(d_model, d_model)
-        self.fc_k = nn.Linear(d_model, d_model)
-        self.fc_v = nn.Linear(d_model, d_model)
-        self.fc_out = nn.Linear(d_model, d_model)
+        self.fc_q = nn.Linear(model_dim, model_dim)
+        self.fc_k = nn.Linear(model_dim, model_dim)
+        self.fc_v = nn.Linear(model_dim, model_dim)
+        self.fc_out = nn.Linear(model_dim, model_dim)
 
     def forward(self, query, key, value, mask=None):
 
         batch_size = query.size(1)
         # linear
-        # (heads, batch_size, seq_len, d_k)
+        # (num_heads, batch_size, seq_len, d_k)
         query, key, value = \
-            [l(x).view(-1, batch_size, self.heads, self.d_k).transpose(0, 2)
+            [l(x).view(-1, batch_size, self.num_heads, self.d_k).transpose(0, 2)
             for l, x in zip((self.fc_q, self.fc_k, self.fc_v), (query, key, value))]
 
         # scaled dot-product attention
-        # (heads, batch_size, seq_len, seq_len)
+        # (num_heads, batch_size, seq_len, seq_len)
         scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(self.d_k)
 
         if mask is not None:
@@ -68,30 +68,30 @@ class MultiHeadAttentionLayer(nn.Module):
         x = torch.matmul(align, value)
 
         # concat
-        # (seq_len, batch_size, d_model)
-        x = x.transpose(0, 2).contiguous().view(-1, batch_size, self.d_model)
+        # (seq_len, batch_size, model_dim)
+        x = x.transpose(0, 2).contiguous().view(-1, batch_size, self.model_dim)
 
         # linear
         return self.fc_out(x)
 
 class EncoderLayer(nn.Module):
 
-    def __init__(self, d_model, d_ff, heads, p_drop):
+    def __init__(self, model_dim, ff_dim, num_heads, drop_prob):
         super(EncoderLayer, self).__init__()
 
-        self.self_attn = MultiHeadAttentionLayer(d_model, heads)
-        self.attn_norm = nn.LayerNorm(d_model)
-        self.dropout1 = nn.Dropout(p_drop)
-        self.fead_forward = PositionWiseFullyConnectedLayer(d_model, d_ff)
-        self.fc_norm = nn.LayerNorm(d_model)
-        self.dropout2 = nn.Dropout(p_drop)
+        self.self_attn = MultiHeadAttentionLayer(model_dim, num_heads)
+        self.attn_norm = nn.LayerNorm(model_dim)
+        self.dropout1 = nn.Dropout(drop_prob)
+        self.fead_forward = PositionWiseFullyConnectedLayer(model_dim, ff_dim)
+        self.fc_norm = nn.LayerNorm(model_dim)
+        self.dropout2 = nn.Dropout(drop_prob)
 
-    def forward(self, x, input_mask):
+    def forward(self, x, src_mask):
 
         # 1st sub-layer
         # Multi-Head Self-Attention
         # residual connection
-        x = x + self.dropout1(self.self_attn(x, x, x, input_mask))
+        x = x + self.dropout1(self.self_attn(x, x, x, src_mask))
         # layer norm
         x = self.attn_norm(x)
 
@@ -105,55 +105,55 @@ class EncoderLayer(nn.Module):
 
 class Encoder(nn.Module):
 
-    def __init__(self, d_model, d_input, num_layers, d_ff, heads, seq_len, p_drop):
+    def __init__(self, input_dim, model_dim, ff_dim, num_layers, num_heads, max_len, drop_prob):
         super(Encoder, self).__init__()
 
-        self.d_model = d_model
+        self.model_dim = model_dim
 
-        self.embedding = nn.Embedding(d_input, d_model)
-        self.encoding = PositionalEncoding(d_model, seq_len)
-        self.dropout = nn.Dropout(p_drop)
-        self.layers = nn.ModuleList([EncoderLayer(d_model, d_ff, heads, p_drop) 
+        self.embedding = nn.Embedding(input_dim, model_dim)
+        self.encoding = PositionalEncoding(model_dim, max_len)
+        self.dropout = nn.Dropout(drop_prob)
+        self.layers = nn.ModuleList([EncoderLayer(model_dim, ff_dim, num_heads, drop_prob) 
                                     for _ in range(num_layers)])
 
-    def forward(self, x, input_mask):
-        # x (seq_len, batch_size)
+    def forward(self, x, src_mask):
+        # x (src_len, batch_size)
 
-        # (seq_len, batch_size, model_dim)
-        embed = self.embedding(x) * math.sqrt(self.d_model)
+        # (src_len, batch_size, model_dim)
+        embed = self.embedding(x) * math.sqrt(self.model_dim)
         encod = self.encoding(embed)
         x = self.dropout(encod)
 
         for layer in self.layers:
-            x = layer(x, input_mask)
+            x = layer(x, src_mask)
 
         return x
 
 class DecoderLayer(nn.Module):
 
-    def __init__(self, d_model, d_ff, heads, p_drop):
+    def __init__(self, model_dim, ff_dim, num_heads, drop_prob):
         super(DecoderLayer, self).__init__()
         
-        self.self_attn = MultiHeadAttentionLayer(d_model, heads)
-        self.self_attn_norm = nn.LayerNorm(d_model)
-        self.dropout1 = nn.Dropout(p_drop)
-        self.enc_attn = MultiHeadAttentionLayer(d_model, heads)
-        self.enc_attn_norm = nn.LayerNorm(d_model)
-        self.dropout2 = nn.Dropout(p_drop)
-        self.fead_forward = PositionWiseFullyConnectedLayer(d_model, d_ff)
-        self.fc_norm = nn.LayerNorm(d_model)
-        self.dropout3 = nn.Dropout(p_drop)
+        self.self_attn = MultiHeadAttentionLayer(model_dim, num_heads)
+        self.self_attn_norm = nn.LayerNorm(model_dim)
+        self.dropout1 = nn.Dropout(drop_prob)
+        self.enc_attn = MultiHeadAttentionLayer(model_dim, num_heads)
+        self.enc_attn_norm = nn.LayerNorm(model_dim)
+        self.dropout2 = nn.Dropout(drop_prob)
+        self.fead_forward = PositionWiseFullyConnectedLayer(model_dim, ff_dim)
+        self.fc_norm = nn.LayerNorm(model_dim)
+        self.dropout3 = nn.Dropout(drop_prob)
 
-    def forward(self, x, encoder_outputs, input_mask, target_mask):
+    def forward(self, x, encoder_outputs, src_mask, tgt_mask):
 
         # 1st sub-layer
         # Multi-Head Self-Attention
-        x = x + self.dropout1(self.self_attn(x, x, x, target_mask))
+        x = x + self.dropout1(self.self_attn(x, x, x, tgt_mask))
         x = self.self_attn_norm(x)
 
         # 2nd sub-layer
         # Encoder Multi-Head Attention
-        x = x + self.dropout2(self.enc_attn(x, encoder_outputs, encoder_outputs, input_mask))
+        x = x + self.dropout2(self.enc_attn(x, encoder_outputs, encoder_outputs, src_mask))
         x = self.enc_attn_norm(x)
 
         # 3rd sub-layer
@@ -165,57 +165,57 @@ class DecoderLayer(nn.Module):
 
 class Decoder(nn.Module):
 
-    def __init__(self, d_model, d_output, num_layers, d_ff, heads, seq_len, p_drop):
+    def __init__(self, output_dim, model_dim, ff_dim, num_layers, num_heads, max_len, drop_prob):
         super(Decoder, self).__init__()
 
-        self.d_model = d_model
+        self.model_dim = model_dim
 
-        self.embedding = nn.Embedding(d_output, d_model)
-        self.encoding = PositionalEncoding(d_model, seq_len)
-        self.dropout = nn.Dropout(p_drop)
-        self.layers = nn.ModuleList([DecoderLayer(d_model, d_ff, heads, p_drop) for _ in range(num_layers)])
-        self.fc_out = nn.Linear(d_model, d_output)
+        self.embedding = nn.Embedding(output_dim, model_dim)
+        self.encoding = PositionalEncoding(model_dim, max_len)
+        self.dropout = nn.Dropout(drop_prob)
+        self.layers = nn.ModuleList([DecoderLayer(model_dim, ff_dim, num_heads, drop_prob)
+                                    for _ in range(num_layers)])
+        self.fc_out = nn.Linear(model_dim, output_dim)
 
-    def forward(self, x, encoder_outputs, input_mask, target_mask):
+    def forward(self, x, encoder_outputs, src_mask, tgt_mask):
 
-        # (seq_len, batch_size, d_model)
-        embed = self.embedding(x) * math.sqrt(self.d_model)
+        # (tgt_len, batch_size, model_dim)
+        embed = self.embedding(x) * math.sqrt(self.model_dim)
         encod = self.encoding(embed)
         x = self.dropout(encod)
 
         for layer in self.layers:
-            x = layer(x, encoder_outputs, input_mask, target_mask)
+            x = layer(x, encoder_outputs, src_mask, tgt_mask)
             
         return F.log_softmax(self.fc_out(x), dim=2)
 
 class Transformer(nn.Module):
 
-    def __init__(self, d_input, d_output, d_model, d_ff, seq_len, num_layers, num_heads, p_drop,
-                src_pad_token, trg_pad_token, device):
+    def __init__(self, src_pad, tgt_pad, max_len, input_dim, output_dim,
+                model_dim, ff_dim, num_layers, num_heads, drop_prob):
         super(Transformer, self).__init__()
 
-        self.encoder = Encoder(d_model, d_input, num_layers, d_ff, num_heads, seq_len, p_drop)
-        self.decoder = Decoder(d_model, d_output, num_layers, d_ff, num_heads, seq_len, p_drop)
+        self.encoder = Encoder(input_dim, model_dim, ff_dim, num_layers, num_heads, max_len, drop_prob)
+        self.decoder = Decoder(output_dim, model_dim, ff_dim, num_layers, num_heads, max_len, drop_prob)
 
-        self.src_pad_token = src_pad_token
-        self.trg_pad_token = trg_pad_token
-        self.device = device
+        self.src_pad = src_pad
+        self.tgt_pad = tgt_pad
 
-    def forward(self, src, trg):
+    def forward(self, src, tgt):
 
-        trg = trg[:-1]
+        tgt = tgt[:-1]
         
         # input mask
         # (1, batch_size, 1, input_len)
-        src_mask = (input == self.src_pad_token).transpose(0, 1).unsqueeze(1).unsqueeze(0)
+        src_mask = (input == self.src_pad).transpose(0, 1).unsqueeze(1).unsqueeze(0)
 
         # target mask
-        trg_len = trg.size(0)
+        tgt_len = tgt.size(0)
         # (target_len, target_len)
-        sub_mask = torch.tril(torch.ones((trg_len, trg_len), device=self.device)) == 0
+        sub_mask = torch.tril(torch.ones((tgt_len, tgt_len), device=tgt.device)) == 0
         # (1, batch_size, 1, target_len)
-        pad_mask = (target == self.trg_pad_token).transpose(0, 1).unsqueeze(1).unsqueeze(0)
+        pad_mask = (target == self.tgt_pad).transpose(0, 1).unsqueeze(1).unsqueeze(0)
         # (1, batch_size, target_len, target_len)
-        trg_mask = sub_mask | pad_mask
+        tgt_mask = sub_mask | pad_mask
 
-        return self.decoder(trg, self.encoder(src, src_mask), src_mask, trg_mask)
+        return self.decoder(tgt, self.encoder(src, src_mask), src_mask, tgt_mask)
